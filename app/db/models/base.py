aae9cf6c2e44abab7ab00967b81455dd3b2a40ec
Joshua Thijssen <jthijssen@noxlogic.nl>
2026-05-20 09:37:03 +0200

    Fixed minor suggestions

--- app/db/models/base.py
+++ app/db/models/base.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
-    pass
+    def __repr__(self) -> str:
+        props = ", ".join([f"{k}={self.__getattribute__(k)}" for k in self.__table__.columns.keys()])
+        return f"<{self.__class__.__name__}=({props})>"
 
 
 TBase = TypeVar("TBase", bound=Base)
